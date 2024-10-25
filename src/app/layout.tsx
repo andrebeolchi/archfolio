e9d3bde152802f3b566fccb201f119accf17f6f2
Andre Beolchi <andregarbini2001@gmail.com>
2024-10-25 00:36:50 -0300

    feat(wip): adds login feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import '@/app/globals.css'
 
 import type { Metadata } from 'next'
+import { User } from 'firebase/auth'
 import localFont from 'next/font/local'
+import { cookies } from 'next/headers'
+import { getTokens, Tokens } from 'next-firebase-auth-edge'
+import { Claims, filterStandardClaims } from 'next-firebase-auth-edge/lib/auth/claims'
 
 import { ThemeProvider } from '@/components/theme-provider'
-import { Toaster } from '@/components/ui/toaster'
+import { Toaster } from '@/components/ui/sonner'
+import { clientConfig, serverConfig } from '@/interfaces/firebase/config'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -22,11 +27,46 @@ export const metadata: Metadata = {
   description: 'Portfolio',
 }
 
-export default function RootLayout({
+const toUser = ({ decodedToken }: Tokens): Partial<User & { customClaims: Claims }> => {
+  const {
+    uid,
+    email,
+    picture: photoURL,
+    email_verified: emailVerified,
+    phone_number: phoneNumber,
+    name: displayName,
+    source_sign_in_provider: signInProvider,
+  } = decodedToken
+
+  const customClaims = filterStandardClaims(decodedToken)
+
+  return {
+    uid,
+    email: email ?? null,
+    displayName: displayName ?? null,
+    photoURL: photoURL ?? null,
+    phoneNumber: phoneNumber ?? null,
+    emailVerified: emailVerified ?? false,
+    providerId: signInProvider,
+    customClaims,
+  }
+}
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const tokens = await getTokens(await cookies(), {
+    apiKey: clientConfig.apiKey,
+    cookieName: serverConfig.cookieName,
+    cookieSignatureKeys: serverConfig.cookieSignatureKeys,
+    serviceAccount: serverConfig.serviceAccount,
+  })
+  const user = tokens ? toUser(tokens) : null
+
+  console.log({ user })
+
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} antialiased `}>
       <body>
