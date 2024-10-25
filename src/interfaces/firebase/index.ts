fa9dfda7db8a864926481ae806b1cc16f24b5d76
Andre Beolchi <andregarbini2001@gmail.com>
2024-10-25 00:36:50 -0300

    feat(wip): adds login feature

--- src/interfaces/firebase/index.ts
+++ src/interfaces/firebase/index.ts
@@ -1,23 +1,14 @@
 import { initializeApp, getApps } from 'firebase/app'
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-}
+import { clientConfig } from './config'
 
-const firebase = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
+const firebase = getApps().length === 0 ? initializeApp(clientConfig) : getApps()[0]
 
 const auth = getAuth(firebase)
 const db = getFirestore(firebase)
 const storage = getStorage(firebase)
-const signIn = (email: string, password: string) => signInWithEmailAndPassword(auth, email, password)
 
-export { firebase, auth, db, storage, signIn }
+export { firebase, auth, db, storage }
