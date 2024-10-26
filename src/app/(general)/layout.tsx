980f99dd8238435efd803c3c84d77b47b5621881
Andre Beolchi <andregarbini2001@gmail.com>
2024-10-26 14:16:47 -0300

    feat(sidebar): changes navbar to sidebar

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -1,27 +1,28 @@
+import { AppSidebar } from '@/components/app-sidebar'
+import { DownloadCurriculumButton, LogoutButton } from '@/components/curriculum-button'
 import { getUser } from '@/components/get-user'
-import { Navigator } from '@/components/navigator'
-import { DownloadCurriculumButton, LogoutButton } from '@/components/navigator/actions'
-import { ModeToggle } from '@/components/theme-mode-toggle'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 
 export default async function GeneralLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const user = await getUser()
 
   return (
-    <div className="w-full space-y-6 flex flex-col flex-1">
-      <div className="w-full flex flex-col">
-        <div className="container p-6 max-w-screen-xl flex items-center gap-6 justify-between">
-          <ModeToggle />
+    <SidebarProvider>
+      <AppSidebar />
+      <div className="w-full space-y-6 flex flex-col flex-1">
+        <div className="w-full flex flex-col">
+          <div className="container p-6 max-w-screen-xl flex items-center gap-6 justify-between">
+            <SidebarTrigger />
 
-          <Navigator />
-
-          {user ? <LogoutButton /> : <DownloadCurriculumButton />}
+            {user ? <LogoutButton /> : <DownloadCurriculumButton />}
+          </div>
+          {/* Sidebar */}
         </div>
-        {/* Sidebar */}
+
+        <div className="container max-w-screen-xl flex flex-1 flex-col py-6">{children}</div>
+
+        {/* TODO -- Footer */}
       </div>
-
-      <div className="container max-w-screen-xl flex flex-1 flex-col py-6">{children}</div>
-
-      {/* TODO -- Footer */}
-    </div>
+    </SidebarProvider>
   )
 }
