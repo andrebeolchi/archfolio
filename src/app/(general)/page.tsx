7a05ea1d5d7d14bd821f503f2901a4e994e255d2
Andre Beolchi <andregarbini2001@gmail.com>
2024-10-26 16:43:01 -0300

    feat(page): remove unnecessary sections

--- src/app/(general)/page.tsx
+++ src/app/(general)/page.tsx
@@ -4,14 +4,6 @@ export default async function Home() {
   return (
     <main>
       <HeroSection />
-
-      <div>
-        <h1 className="text-xl mb-4">Academics</h1>
-      </div>
-
-      <div>
-        <h1 className="text-xl mb-4">Projects</h1>
-      </div>
     </main>
   )
 }
