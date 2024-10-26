e9519a50952fea157e7315173ac1b3257d26ac8b
Andre Beolchi <andregarbini2001@gmail.com>
2024-10-26 12:40:47 -0300

    feat(ui): adds logout functionality

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -1,17 +1,20 @@
+import { getUser } from '@/components/get-user'
 import { Navigator } from '@/components/navigator'
+import { DownloadCurriculumButton, LogoutButton } from '@/components/navigator/actions'
 import { ModeToggle } from '@/components/theme-mode-toggle'
 
 export default async function GeneralLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const user = await getUser()
+
   return (
     <div className="w-full space-y-6 flex flex-col flex-1">
       <div className="w-full flex flex-col">
-        <div className="container py-2 max-w-screen-xl flex items-center gap-6 justify-between">
+        <div className="container p-6 max-w-screen-xl flex items-center gap-6 justify-between">
           <ModeToggle />
 
           <Navigator />
 
-          {/* isLogged ? Logout : Download Curriculum */}
-          <div></div>
+          {user ? <LogoutButton /> : <DownloadCurriculumButton />}
         </div>
         {/* Sidebar */}
       </div>
